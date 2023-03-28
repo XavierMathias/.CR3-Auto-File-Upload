c530841e9032fe184fe1da0450f0e990f6e846c4
David Santos <dsdsantos973@gmail.com>
2023-03-27 20:55:54 -0600

    add if then statements

--- bash.sh
+++ bash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "What is your name?"
 read name
-echo "WHAT?! $name is my name too!"
+if [ "$name" ]; then
+ echo "$name sounds alright to me"
+else
+ echo "I said what is your name BOOIIIII?!"
+fi
